e29642ab1c2c0d842ddc33281eb38a6c7c4da7e0
zhangwentao <zhangwentao@mrjapp.net>
2016-08-01 16:49:48 +0800

    no message

---
+++ downConfig.php
@@ -0,0 +1,9 @@
+<?php
+$filename = "udid.mobileconfig";
+header('Content-Type:application/x-apple-aspen-config'); //指定下载文件类型
+header('Content-Disposition: attachment; filename="'.$filename.'"'); //指定下载文件的描述
+header('Content-Length:'.filesize($filename)); //指定下载文件的大小
+
+//将文件内容读取出来并直接输出，以便下载
+readfile($filename);
+?>
